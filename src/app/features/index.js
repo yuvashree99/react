d39025b46268f093865cdf051273456e85086992
yuvashree99 <57143075+yuvashree99@users.noreply.github.com>
2021-04-26 11:12:17 +0530

    Add files via upload

---
+++ src/app/features/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { store } from './app/store.js';
+
+import { Provider } from 'react-redux';
+
+
+ReactDOM.render(
+  
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+  
+);
---
+++ src/app/features/score/Score.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {useSelector} from 'react-redux';
+import {selectMatchedIDs} from '../board/boardSlice.js';
+export const Score = () => {
+  
+  const cardsMatched=useSelector(selectMatchedIDs);
+
+  return (
+    
+    <div className="score-container">Matched: {cardsMatched.length}</div>
+  );
+};
